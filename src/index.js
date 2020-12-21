98e94cbb8459035c8dbd7a8cc02cceef492f0eb1
Brian <a@atomrigs.io>
2020-12-20 19:57:28 -0600

    add bet ui

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ app.post('/api/v1/getHash', async (req, res, next) => {
     const { contract, pollId, choice, address } = req.body
     assert (contract.length == 42, 'The length of contract address should be 42.')
     assert (parseInt(pollId) >= 0 && parseInt(pollId) < 10000, 'The pollId should be less than 10000.')
-    assert (parseInt(choice) >= 1 && parseInt(choice) < 11), 'The number of choices should be less than 11.'
+    assert (parseInt(choice) >= 1 && parseInt(choice) < 11, 'The number of choices should be less than 11.')
     assert (address.length == 42, 'The address length should be 42.')
     const dbKey = ethers.utils.id(`saltHash-${contract}-${pollId}`)
     let pollSalt 
